1a24b3597dc7794189ed88a318371c93edf95dd8
Chris Pieper <chris.pieper.61@gmail.com>
2020-04-27 11:25:40 -0400

    Linted with flake8 and pylint.

--- webapp/app.py
+++ webapp/app.py
@@ -1,26 +1,22 @@
 """ This is the main application file. """
-from flask import Flask, abort, flash, render_template, redirect, request, url_for
-from flask_bootstrap import Bootstrap
-from flask.logging import default_handler
-
-import auth
 import logging
-import os
-import relay
-import time
-
 from logging.handlers import TimedRotatingFileHandler
 
-handler = TimedRotatingFileHandler("log/garage_opener.log",
-                                    when="midnight",
-                                    interval=1,
-                                    backupCount=7)
-logging.getLogger('werkzeug').addHandler(handler)
+from flask import Flask, abort, flash, redirect, render_template
+from flask import request, url_for
+from flask_bootstrap import Bootstrap
+
+import auth
+import relay
+
+HANDLER = TimedRotatingFileHandler("log/garage_opener.log",
+                                   when="midnight", interval=1, backupCount=7)
+logging.getLogger('werkzeug').addHandler(HANDLER)
 APP = Flask(__name__)
 APP.logger.setLevel(logging.INFO)
-APP.logger.addHandler(handler)
+APP.logger.addHandler(HANDLER)
 
-bootstrap = Bootstrap(APP)
+Bootstrap(APP)
 APP.config.from_pyfile("config.py")
 
 LEFT = 0
@@ -28,20 +24,22 @@ RIGHT = 1
 RELAYS = {"Left": LEFT, "Right": RIGHT}
 MAC_LIST = APP.config['MAC_LIST']
 
+
 @APP.route("/", methods=["POST", "GET"])
 def index():
     """ This processes the default route. """
     if request.method == "GET":
         return render_template("index.html")
 
-    if (request.method == "POST") and (auth.check(request.remote_addr, MAC_LIST, APP.logger)):
+    if (request.method == "POST") and \
+            (auth.check(request.remote_addr, MAC_LIST, APP.logger)):
         action = request.form["action"]
         if action in RELAYS.keys():
             relay.press(RELAYS[action])
             flash(action + " button pressed")
-            APP.logger.warning('%s button pressed by %s', 
-                action, 
-                auth.get_mac(request.remote_addr))
+            APP.logger.warning('%s button pressed by %s',
+                               action,
+                               auth.get_mac(request.remote_addr))
             return redirect(url_for('index'))
         else:
             APP.logger.error('Action other than Left or Right')
@@ -50,13 +48,18 @@ def index():
         APP.logger.warning('POST attempted by %s', request.remote_addr)
         abort(403)
 
+
 @APP.errorhandler(403)
-def forbidden(e):
+def forbidden():
+    " When an forbidden access occurs render the custom error page. "
     return render_template('403.html'), 403
 
+
 @APP.errorhandler(500)
-def internal_server_error(e):
+def internal_server_error():
+    " When an internal server error occurs render the custom error page. "
     return render_template('500.html'), 500
+
 
 if __name__ == "__main__":
     relay.init()
--- webapp/auth.py
+++ webapp/auth.py
@@ -1,11 +1,9 @@
 """ Authentication of a local activation."""
-from flask import jsonify
-import logging
 import os
-import sys
+
 
 def check(ip_address, mac_list, logger):
-    """ Check the IP for being within the local network. """
+    " Check the IP for being within the local network. "
 
     if '192.168.1' in ip_address:
         mac = get_mac(ip_address)
@@ -17,12 +15,15 @@ def check(ip_address, mac_list, logger):
     else:
         return False
 
+
 def get_mac(ip_address):
+    " Strip the mac (hardware) address from the result of 'arp'. "
+
     arp_string = os.popen('arp -a | grep ' + ip_address).read()
     if ip_address in arp_string:
-        left, arp_string = arp_string.split('at ', 1)
+        _, arp_string = arp_string.split('at ', 1)
         if 'on' in arp_string:
-            mac, right = arp_string.split(' [ether]', 1)
+            mac, _ = arp_string.split(' [ether]', 1)
             mac = mac.strip()
             return mac
         return None
